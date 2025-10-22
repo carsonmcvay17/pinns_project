ca6c428b0f8c4f875b6af31b8b5821d93bc82580
carsonmcvay17 <carsonmcvay@utexas.edu>
2025-10-22 12:51:43 -0500

    numerical sol for y5

--- troesch/gen_data.py
+++ troesch/gen_data.py
@@ -1,16 +1,26 @@
 # imports
 import numpy as np
+from scipy.optimize import root_scalar
 
 class GenData:
     """
     Class that generates data to solve the troesch problem
     """ 
 
-    def true_sol(self, x_colloc, s, lam):
+    def true_sol(self, x, lam):
         """
         Reutrns the solution to the Troesch problem according to the paper 
         Solution approximated by polynomials
         y_5(x)= sx + 1/6 lambda^2sx^3 + (lambda^4s)/120(1+s^2)x^5
         Find parameter s by solving y_5(1)=1 for fixed lambda
         """
-        return s * x_colloc + 1/6 * (lam**2 * s * x_colloc**3) + ((lam**4 * s) / 120) * (1 + s**2) * x_colloc**5
+        x = np.array(x)
+        
+        def f(s):
+            return s + (1/6)*lam**2*s + (lam**4*s/120)*(1+s**2) - 1
+        
+        sol = root_scalar(f, bracket=[1e-6, 2], method='brentq')
+        s = sol.root
+
+        y5 = s * x + (1/6) * (lam**2 * s * x**3) + ((lam**4 * s) / 120) * (1 + s**2) * x**5
+        return y5
